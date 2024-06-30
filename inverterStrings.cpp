a89d25cd75a99fd90c0c673fa18eb8601a10d459
vnizio <viniciuscastro2615@gmail.com>
2024-06-30 20:18:57 -0300

    inverter atráves de pilha

---
+++ inverterStrings.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(){
+    stack<string> pilha;
+
+    cout << "digite linhas terminando com linha vazia\n\n";
+
+    for(;;){
+
+        string linha; getline(cin,linha);
+
+        if(linha == "") break;
+
+        pilha.push(linha);
+
+    }
+
+    cout << "\nlinhas ordem inversa\n";
+
+    while(not pilha.empty()){
+        cout << pilha.top() << "\n";
+        pilha.pop();
+    }
+}
